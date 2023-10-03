d92452fea6d772334154bd3c6e1324646663cff5
Gustavo Okamoto Ventura <gustavoventura-ieg@instituto.ad>
2023-10-03 17:07:20 -0300

    logica de mudar lista feita

--- src/Pages/FavoritesCentral/index.tsx
+++ src/Pages/FavoritesCentral/index.tsx
@@ -1,10 +1,18 @@
 import {useState, useEffect} from 'react'
 import * as S from './style'
+
+import Radio from '@mui/material/Radio';
+import RadioGroup from '@mui/material/RadioGroup';
+import FormControlLabel from '@mui/material/FormControlLabel';
+import FormControl from '@mui/material/FormControl';
+import FormLabel from '@mui/material/FormLabel';
+
 import { PageWrapper } from '../Home/styles'
 import { TopBar } from '../../Components/TopBar'
 
-import {sliderImageUrl} from '../Home/mock'
+import { sliderImageUrl, products } from '../Home/mock'
 import { LocationCard } from '../../Components/LocationCard'
+import { ProductCard } from '../../Components/ProductCard';
 
 interface ILocationsProps{
     url: string
@@ -13,8 +21,16 @@ interface ILocationsProps{
     rating: number
 }
 
+interface IProductsProps{
+    section?: string
+    img_url?: string
+    price?: number
+}
+
 const FavoritesCentral = () => {
     const [favoriteLocations, setFavoriteLocations] = useState<ILocationsProps[]>()
+    const [favoriteProducts, setFavoriteProducts] = useState<IProductsProps[]>()
+    const [optionSelected, setOptionsSelected] = useState(1)
 
     useEffect(() => {
         if(sliderImageUrl?.length){
@@ -22,23 +38,56 @@ const FavoritesCentral = () => {
         }
     }, [sliderImageUrl])
 
+    useEffect(() => {
+        if(products?.length){
+            setFavoriteProducts(products)
+        }
+    }, [products])
+
     return(
         <>
             <PageWrapper>
                 <TopBar />
-                <S.TypographyComponent fontPeso={'600'} tamanho={'1.5rem'}>Bem-vindo à sua central de favoritos! Veja os lugares e anúncios que você deu destaque!</S.TypographyComponent>
+                <S.TypographyComponent fontPeso={'600'} tamanho={'1.5rem'}>Bem-vindo à sua central de favoritos! Veja os {optionSelected == 1 ? "lugares" : "produtos"} que você deu destaque!</S.TypographyComponent>
                 <S.SearchContainer>
                     <S.SearchInput placeholder='Pesquisar em meus favoritos...'/>
                     <S.IconSearch />
                 </S.SearchContainer>
+                <FormControl>
+                    <RadioGroup
+                        aria-labelledby="demo-radio-buttons-group-label"
+                        defaultValue="Estabelecimentos"
+                        name="radio-buttons-group"
+                    >
+                        <FormControlLabel value="Estabelecimentos" control={<Radio onChange={() => setOptionsSelected(1)}/>} label="Estabelecimentos" />
+                        <FormControlLabel value="Produtos" control={<Radio onChange={() => setOptionsSelected(2)}/>} label="Produtos" />
+                    </RadioGroup>
+                </FormControl>
                 <div style={{ display: 'flex', flexDirection: 'column', gap: '0.7rem', width: '100%' }}>
-                    <S.TypographyComponent fontPeso={'600'} tamanho={'1.2rem'}>Meus Lugares Favoritos:</S.TypographyComponent>
+                    <S.TypographyComponent fontPeso={'600'} tamanho={'1.2rem'}>Meus {optionSelected == 1 ? "Lugares" : "Produtos"} Favoritos:</S.TypographyComponent>
                     <S.LocationContainer>
-                        {favoriteLocations?.length && <>
-                            {favoriteLocations.map((loc) => {
-                                return(<LocationCard imgUrl={loc.url} localName={loc.name} location={loc.endereco} ratingValue={loc.rating}/>)
-                            })}
-                        </>}
+                        {optionSelected == 1 ? 
+                            <>
+                                {!!favoriteLocations?.length && <>
+                                    {favoriteLocations.map((loc, index) => {
+                                        return(<LocationCard imgUrl={loc.url} localName={loc.name} location={loc.endereco} ratingValue={loc.rating} key={index}/>)
+                                    })}
+                            </>
+                        }
+                        </> : 
+                        <>
+                            {!!favoriteProducts?.length && 
+                                <>
+                                    {favoriteProducts.map((prod, index) => {
+                                        return(
+                                            <ProductCard key={index} productImg={prod.img_url} sectionName={prod.section} productPrice={prod.price}/>
+                                        )
+                                    })}
+                                </>
+                            }
+                        </>
+                        }
+                        
                     </S.LocationContainer>
                 </div>
             </PageWrapper>        
