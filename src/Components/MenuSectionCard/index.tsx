9eaa4eed0c1d8ea6054ee8fef82705fbbc0cdf4f
Gustavo Okamoto Ventura <gustavoventura-ieg@instituto.ad>
2023-10-03 17:27:21 -0300

    redirect para favoritos

--- src/Components/MenuSectionCard/index.tsx
+++ src/Components/MenuSectionCard/index.tsx
@@ -1,16 +1,20 @@
 // import React from 'react'
 import * as S from './styles'
 import ArrowForwardIosIcon from '@mui/icons-material/ArrowForwardIos';
+import { useNavigate } from 'react-router-dom';
 
 export interface IMenuSecionCardProps{
     image?: any
     text?: string
+    urlToNavigate?: string
 }
 
-const MenuSectionCard = ({text, image}: IMenuSecionCardProps) => {
+const MenuSectionCard = ({text, image, urlToNavigate}: IMenuSecionCardProps) => {
+    const navigate = useNavigate()
+    
     return(
         <>
-            <S.Container>
+            <S.Container onClick={() => navigate(urlToNavigate || '')}>
                 <S.IconImage src={image}/>
                 <S.TypographyComponent>{text}</S.TypographyComponent>
                 <S.CardButton variant='contained'>
